9f8668c9aa56dd9d25534dc545a84b2ba63ab002
MatthewSteen <MatthewSteen@users.noreply.github.com>
2023-10-26 19:14:20 +0000

    update measures gbxml

---
+++ measures/systems_analysis_report_generator/resources/eplusout/mappers/mapper.rb
@@ -0,0 +1,31 @@
+﻿module EPlusOut
+  module Mappers
+    class Mapper
+      attr_reader :sql_file
+
+      def param_map
+        raise NotImplementedError, 'Must be implemented by child class'
+      end
+
+      def klass
+        raise NotImplementedError, 'Must be implemented by child class'
+      end
+
+      def size
+        return param_map.size
+      end
+
+      def call(data)
+        result = klass.new
+
+        if data
+          param_map.each do |param|
+            result.send("#{param[:name]}=", data[param[:index]])
+          end
+        end
+
+        result
+      end
+    end
+  end
+end
